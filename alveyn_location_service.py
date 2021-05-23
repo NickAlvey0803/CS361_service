76b5b6a3a7aedeebdcf64f891b54b16dab2dcb60
NickAlvey0803 <59594631+NickAlvey0803@users.noreply.github.com>
2021-05-23 09:02:07 -0700

    Add files via upload

--- alveyn_location_service.py
+++ alveyn_location_service.py
@@ -33,7 +33,7 @@ def return_location():
 
     string_dict["longitude"] = string_list[string_list.index('Longitude:') + 1]
     string_dict["latitude"] = string_list[string_list.index('Latitude:') + 1]
-    # string_dict["IP address"] = string_list[string_list.index('IP Address:') + 1]
+    string_dict["IP address"] = string_list[string_list.index('IP Address:') + 1]
     string_dict["City"] = string_list[string_list.index('City:') + 1]
     string_dict["Region"] = string_list[string_list.index('Region:') + 1]
     string_dict["Zip"] = string_list[string_list.index('Postal Code:') + 1]
